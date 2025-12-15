ee4711ecd3a279c675609e51427251d877532dad
Rafael540 <alves540@hotmail.com>
2025-12-15 15:54:04 -0300

    controle

--- Med.Voll/src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ Med.Voll/src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -1,8 +1,7 @@
-package med.voll.api.endereco;
+package med.voll.api.domain.endereco;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -35,5 +34,29 @@ public class Endereco {
         this.uf = dados.uf();
         this.cidade = dados.cidade();
 
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+    if(dados.logradouro() != null) {
+        this.logradouro = dados.logradouro();
+    }
+    if(dados.bairro() != null) {
+        this.bairro = dados.bairro();
+    }
+    if( dados.cep() != null) {
+        this.cep = dados.cep();
+    }
+    if(dados.uf() != null) {
+        this.uf = dados.uf();
+    }
+    if(dados.cidade() != null) {
+        this.cidade = dados.cidade();
+    }
+    if(dados.numero() != null) {
+        this.numero = dados.numero();
+    }
+    if(dados.complemento() != null) {
+        this.complemento = dados.complemento();
+    }
     }
 }
